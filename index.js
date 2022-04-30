eaf3ec1cfa87026afac76625d6d724fe0c668cd8
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-29 22:47:23 -0700

    Added ASCII Art to displayDB function

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const mysql = require('mysql2');
 
 // Placeholder Function to display the datatbase quieries
 function displayDB() {
-    console.log(`-----------------------------------------`);
-    console.log(`Welcome to the Employee Management System`);
-    console.log(`-----------------------------------------`);
+    console.log("+-----------------------------------------------+");
+    console.log("|  _____           _                            |");
+    console.log("| |   __|_____ ___| |___ _ _ ___ ___            |");
+    console.log("| |   __|     | . | | . | | | -_| -_|           |");
+    console.log("| |_____|_|_|_|  _|_|___|_  |___|___|           |");
+    console.log("|             |_|       |___|                   |");
+    console.log("|  _____                                   _    |");
+    console.log("| |     |___ ___ ___ ___ ___ _____ ___ ___| |_  |");
+    console.log("| | | | | .'|   | .'| . | -_|     | -_|   |  _| |");
+    console.log("| |_|_|_|__,|_|_|__,|_  |___|_|_|_|___|_|_|_|   |");
+    console.log("|                   |___|                       |");
+    console.log("+-----------------------------------------------+");
+    console.log("");
     console.log(`Select from the list what you'd like to do`);
+    console.log("");
 }
 
 function mainMenu() {
