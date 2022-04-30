ab7f43f57555f27cf5026381780a2c7d7dc0387a
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-29 22:21:31 -0700

    Added mainMenu() function and pseudocode file

---
+++ pseudocode.js
@@ -0,0 +1,26 @@
+// GAME PLAN
+// Make an inquirer prompt, that branches into other prompts.
+// -------------------------
+// Main menu prompt function
+    // View All Employees
+        // Display All Employees
+        // Repeat menu prompt
+    // Add Employee
+        // IF there's a department AND IF there's an employee type available
+        // Repeat menu prompt
+    // Update Employee Role
+        // If there's a department
+        // If the role that they want to change exists
+            // Then select the role and change to what the input is
+
+    // View All Roles
+        // select all roles from all departmets then print them
+    // Add Role
+        // Add role to table
+
+    // View All Departments
+        // Display All Departments
+        // Repeat menu prompt
+    // Add Department
+        // Insert department into db
+    // Quit
--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-// GAME PLAN
-// Make an inquirer prompt, that branches into other prompts.
-// -------------------------
-// Main menu prompt function
-    // View All Employees
-        // Display All Employees
-        // Repeat menu prompt
-    // Add Employee
-        // IF there's a department AND IF there's an employee type available
-        // Repeat menu prompt
-    // Update Employee Role
-        // If there's a department
-        // If the role that they want to change exists
-            // Then select the role and change to what the input is
-
-    // View All Roles
-        // select all roles from all departmets then print them
-    // Add Role
-        // Add role to table
-
-    // View All Departments
-        // Display All Departments
-        // Repeat menu prompt
-    // Add Department
-        // Insert department into db
-    // Quit
-    
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+function mainMenu() {
+    console.log(`Welcome to the Employee Management System`);
+    inquirer.prompt([{
+        type: `list`,
+        message: `Main Menu`,
+        choices: [
+        `View All Employees`,
+        `Add Employee`,
+        `Update Employee Role`,
+        `View All Roles`,
+        `Add Role`,
+        `View All Departments`,
+        `Add Department`,
+        `Quit`],
+        name: `menu`
+    }])
+    .then((answer) => {
+        
+    })
+}
